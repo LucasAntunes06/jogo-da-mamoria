72c5ac26290878c87d116f999b7d1e51c8503a23
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-07-20 10:06:34 -0300

    add: dark mode

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const criarTabuleiro = (max = 16) => {
- if (max % 2 == 1) {
-  console.log("O número tem que ser par!");
-  return;
- }
- const tab = [];
+    if (max % 2 == 1) {
+        console.log("O número tem que ser par!");
+        return;
+    }
+    const tab = [];
 
- let k = 0;
- while (k < max) {
-  const n = Math.ceil(Math.random() * (max / 2));
-  if (tab.filter((x) => x === n).length >= 2) continue;
+    let k = 0;
+    while (k < max) {
+        const n = Math.ceil(Math.random() * (max / 2));
+        if (tab.filter((x) => x === n).length >= 2) continue;
 
-  tab.push(n);
-  k++;
- }
+        tab.push(n);
+        k++;
+    }
 
- return tab;
+    return tab;
 };
 
 const jogadores = ["Jogador 1", "Jogador 2"];
@@ -23,17 +23,18 @@ let jogador = 0;
 let jogadas = [];
 let placar = [];
 let trancarJogo = false;
+const mudarTemaBotao = document.getElementById('tema')
 
 const divJ = document.getElementById("jogadores");
 jogadores.forEach((j) => {
- const span = document.createElement("span");
- span.innerHTML = j;
- const i = document.createElement("I");
- i.innerHTML = "0";
- placar.push(i);
- span.appendChild(i);
- divJ.appendChild(span);
- spans.push(span);
+    const span = document.createElement("span");
+    span.innerHTML = j;
+    const i = document.createElement("I");
+    i.innerHTML = "0";
+    placar.push(i);
+    span.appendChild(i);
+    divJ.appendChild(span);
+    spans.push(span);
 });
 
 spans[jogador].classList.add("jogadorAtivo");
@@ -41,51 +42,85 @@ spans[jogador].classList.add("jogadorAtivo");
 const tab = criarTabuleiro();
 const div = document.getElementById("tabuleiro");
 tab.forEach((n) => {
- const btn = document.createElement("button");
- btn.setAttribute("type", "button");
- btn.addEventListener("click", () => {
-  if (trancarJogo) return;
+    const btn = document.createElement("button");
+    btn.setAttribute("type", "button");
+    btn.addEventListener("click", () => {
+        if (trancarJogo) return;
 
-  btn.innerHTML = n;
-  if (jogadas.some((j) => j === btn)) return;
-  jogadas.push(btn);
+        btn.innerHTML = n;
+        if (jogadas.some((j) => j === btn)) return;
+        jogadas.push(btn);
 
-  if (jogadas.length === 2) {
-   trancarJogo = true;
+        if (jogadas.length === 2) {
+            trancarJogo = true;
 
-   if (jogadas[0].innerHTML === jogadas[1].innerHTML) {
-    placar[jogador].innerHTML = parseInt(placar[jogador].innerHTML) + 1;
-    jogadas[0].disabled = true;
-    jogadas[1].disabled = true;
-    trancarJogo = false;
+            if (jogadas[0].innerHTML === jogadas[1].innerHTML) {
+                placar[jogador].innerHTML = parseInt(placar[jogador].innerHTML) + 1;
+                jogadas[0].disabled = true;
+                jogadas[1].disabled = true;
+                trancarJogo = false;
 
-    const p1 = parseInt(placar[0].innerHTML);
-    const p2 = parseInt(placar[1].innerHTML);
+                const p1 = parseInt(placar[0].innerHTML);
+                const p2 = parseInt(placar[1].innerHTML);
 
-    if (p1 + p2 === 8) {
-     if (p1 > p2) {
-      alert("Jogador 1 venceu");
-     } else if (p1 < p2) {
-      alert("Jogador 2 venceu");
-     } else {
-      alert("O jogo terminou empatado!");
-     }
-    }
+                if (p1 + p2 === 8) {
+                    if (p1 > p2) {
+                        alert("Jogador 1 venceu");
+                        location.reload()
+                    } else if (p1 < p2) {
+                        alert("Jogador 2 venceu");
+                        location.reload()
+                    } else {
+                        alert("O jogo terminou empatado!");
+                        location.reload()
+                    }
+                }
 
-    console.log("Você acertou!");
-    jogadas = [];
-   } else {
-    console.log("Você errou!");
-    jogador = (jogador + 1) % jogadores.length;
-    spans.forEach((s) => s.classList.remove("jogadorAtivo"));
-    spans[jogador].classList.add("jogadorAtivo");
-    setTimeout(() => {
-     jogadas.forEach((b) => (b.innerHTML = ""));
-     jogadas = [];
-     trancarJogo = false;
-    }, 1000);
-   }
-  }
- });
- div.appendChild(btn);
+                console.log("Você acertou!");
+                jogadas = [];
+            } else {
+                console.log("Você errou!");
+                jogador = (jogador + 1) % jogadores.length;
+                spans.forEach((s) => s.classList.remove("jogadorAtivo"));
+                spans[jogador].classList.add("jogadorAtivo");
+                setTimeout(() => {
+                    jogadas.forEach((b) => (b.innerHTML = ""));
+                    jogadas = [];
+                    trancarJogo = false;
+                }, 1000);
+            }
+        }
+    });
+    div.appendChild(btn);
 });
+
+const mudarTema = () => {
+    button = document.getElementById('tema')
+    const temaIcon = document.getElementById('tema-icon')
+    const body = document.querySelector('body')
+    const tabuleiro = document.querySelector('#tabuleiro')
+
+    if (!button.classList.contains('dark')) {
+        button.classList.add('dark')
+
+        body.classList.add('dark')
+
+        temaIcon.classList.remove('fa-moon')
+        temaIcon.classList.add('fa-sun')
+
+        tabuleiro.classList.add('dark')
+    }
+    else if (button.classList.contains('dark')) {
+        button.classList.remove('dark')
+
+        body.classList.remove('dark')
+
+        temaIcon.classList.remove('fa-sun')
+        temaIcon.classList.add('fa-moon')
+
+        tabuleiro.classList.remove('dark')
+    }
+}
+
+
+mudarTemaBotao.addEventListener('click', mudarTema)
